c575ce20a233e80cfa80028a44359f50b724e20d
swsobolev <swsobolev@mail.ru>
2022-11-16 13:48:48 +0100

    16.11.

---
+++ Assets/Scenes/BasicMath.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMath : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a;
+        int b;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
